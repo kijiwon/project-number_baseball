11b84fa182ad121d57afa9aae26d115968b2afb6
kijiwon <119961147+kijiwon@users.noreply.github.com>
2023-02-26 21:44:27 +0900

    시작버튼&랜덤번호만들기

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
 import './App.css';
-
+import StartGame from './components/StartGame';
+import { useEffect, useState } from 'react';
 function App() {
+  // 랜덤한 4자리 숫자 뽑기
+  function getNumbers() {
+    const candidate = [1,2,3,4,5,6,7,8,9];
+    const array = [];
+    for (let i = 0; i < 4; i += 1) {
+      const chosen = candidate.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
+      array.push(chosen);
+    }
+    return array;
+  }
+  let initialState = {
+    result: '',
+    value: '',
+    answer: getNumbers(),
+    tries: [],
+  };
+  const [state, setState]= useState(initialState);
+  console.log(state.answer);
+  //play 버튼 클릭시 화면 바꾸기
+  const [className,setClassName]=useState('show-main');
+  const [startGame, setStartGame] = useState(false);
+  useEffect(() => {
+    setClassName(`${startGame ? "show-game" : "show-main"}`);
+  }, [startGame]);
+
   return (
     <div className="App">
+      <div className={className}>
+      <button className='btn' onClick={()=>{
+        setStartGame(startGame=>!startGame);
+        getNumbers();}
+        }>Play</button>
+      </div>
+      <StartGame/>
     </div>
   );
 }
---
+++ src/components/StartGame.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function StartGame (){
+    return(
+        <div>
+            게임을 시작하지
+        </div>
+    )
+};
+
+export default StartGame;
