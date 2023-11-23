2758a660da9b8b251a4136041a96dc68f33a4675
kijiwon <kiji1313@gmail.com>
2023-11-23 16:08:13 +0900

    StartGame 페이지 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import StartGame from './pages/StartGame';
+import Game from './pages/Game';
+import styled from 'styled-components';
+import { SIZE, COLOR } from '../src/style/theme';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+const AppWrapper = styled.div`
+  background-color: ${COLOR.main_green};
+  min-width: ${SIZE.mobileMin};
+  height: 100vh;
+`;
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <AppWrapper>
+        <Routes>
+          <Route path="/" element={<StartGame />} />
+          <Route path="/game" element={<Game />} />
+        </Routes>
+      </AppWrapper>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/style/theme.js
+++ src/style/theme.js
@@ -1,14 +1,14 @@
 export const SIZE = {
-  mobileMin: '360px',
-  mobileMax: '767px',
-  tablet: '768px',
-  desktop: '1024px',
+  mobileMin: `360px`,
+  mobileMax: `767px`,
+  tablet: `768px`,
+  desktop: `1024px`,
 };
 
 export const COLOR = {
   main_green: `#008037`,
   main_yellow: `#FFF2C2`,
   hover_green: `#309B4F`,
-  hover_yellow: `#FCE89F`,
+  hover_yellow: `#FFE37B`,
   gray_green: `#94BF88`,
 };
