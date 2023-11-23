1593fbcb443c9c3cf5e8a13225e19e08813dbdbc
kijiwon <kiji1313@gmail.com>
2023-11-23 14:24:23 +0900

    theme 설정, 로고 추가

---
+++ src/style/theme.js
@@ -0,0 +1,14 @@
+export const SIZE = {
+  mobileMin: '360px',
+  mobileMax: '767px',
+  tablet: '768px',
+  desktop: '1024px',
+};
+
+export const COLOR = {
+  main_green: `#008037`,
+  main_yellow: `#FFF2C2`,
+  hover_green: `#309B4F`,
+  hover_yellow: `#FCE89F`,
+  gray_green: `#94BF88`,
+};
